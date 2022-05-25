aa5bbff8dec911240b618a9f1ce9e996f4c6cb7e
humblemojo <vanditsharma001@gmail.com>
2022-05-25 00:20:42 -0700

    Added a generic logger utility

---
+++ bash/Logger/README.md
@@ -0,0 +1,18 @@
+##  Bash Logger
+
+-------------------------------------------
+A comprehensive reusable logger utility in bash.
+
+Usage: `source logger.sh` 
+
+Refer this the script where logging is required.
+
+Exposed methods :
+- info
+- debug
+- error
+-------------------------------------------
+
+Todos :
+- Color Highlighting
+
---
+++ bash/Logger/logger.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+#------------------------------------------------------------
+#Logging utility with color coded messages
+#
+#------------------------------------------------------------
+
+LOG_COL_BLACK='\033[0;30m'
+LOG_COL_RED='\033[0;31m'
+LOG_COL_GREEN='\033[0;32m'
+LOG_COL_YELLOW='\033[0;33m'
+LOG_COL_BLUE='\033[0;34m'
+LOG_COL_PURPLE='\033[0;35m'
+LOG_COL_CYAN='\033[0;36m'
+
+
+
+function log_info() {
+  TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+  printf "${LOG_COL_BLUE}INFO${NC}:::${LOG_COL_PURPLE}$TIMESTAMP${NC}:::$1\n"
+}
+
+
